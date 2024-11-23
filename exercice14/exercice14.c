aeed63b6ad591eedae5b7f870f5e7ef988f99a26
Takowski <93830775+Takowski@users.noreply.github.com>
2024-11-23 11:18:31 +0100

    exercice 14 correction

--- exercice14/exercice14.c
+++ exercice14/exercice14.c
@@ -2,11 +2,11 @@
 
 int main(int argc, char const *argv[])
 {
-   printf("Ecris un nombre !\n ");
-   double number=0;
-   scanf("%lf",&number);
-   double square=number*number;
-   printf("%.2f\n",square);
+    printf("Ecris un nombre !\n ");
+    double number = 0;
+    scanf("%lf", &number);
+    double square = number * number;
+    printf("le carré de %g est %g\n", number, square);
 
     return 0;
 }
